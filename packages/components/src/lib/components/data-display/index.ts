f90c922f4534e439fd25b0992478a53d44605a9b
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-11 22:06:18 +0200

    wip

--- packages/components/src/lib/components/data-display/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './badge/index.js';
-export * from './code-block/index.js';
-export * from './separator/index.js';
--- packages/components/src/lib/components/surfaces/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './accordion/index.js';
-export * from './paper/index.js';
