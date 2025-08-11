f024a34db5180013b28cf021d17f2ee7677c29ad
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-11 23:33:10 +0200

    wip

--- packages/components/src/lib/components/backdrop/Backdrop.svelte
+++ packages/components/src/lib/components/backdrop/Backdrop.svelte
@@ -3,6 +3,7 @@
 	import { backdrop } from './variants.js';
 	import type { Snippet } from 'svelte';
 	import type { WithTWMergeClass } from '$lib/types.js';
+	import { on } from 'svelte/events';
 
 	export interface BackdropProps extends WithTWMergeClass {
 		children?: Snippet;
@@ -14,11 +15,20 @@
 	}
 
 	let { children, class: className, fadeDuration = 150, z = 10, onClick }: BackdropProps = $props();
+
+	let ref = $state<HTMLDivElement>();
+
+	function onclick(event: MouseEvent) {
+		if (event.target !== ref) return;
+
+		onClick?.();
+	}
 </script>
 
 <div
+	bind:this={ref}
 	transition:fade={{ duration: fadeDuration }}
-	onclick={onClick}
+	{onclick}
 	aria-hidden={true}
 	style="z-index: {z}"
 	class={backdrop({ className })}
