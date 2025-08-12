b6c47e3d2b4ceb66ce4c89c0934de0e3e81fb0f1
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-12 08:30:44 +0200

    wip

--- packages/components/src/lib/components/select/Option.svelte
+++ packages/components/src/lib/components/select/Option.svelte
@@ -4,8 +4,11 @@
 	import IconCheck from '@tabler/icons-svelte/icons/check';
 	import type { HTMLAttributes } from 'svelte/elements';
 	import type { ClassNameValue } from 'tailwind-merge';
+	import { useMutationObserver } from '$utils/use-mutation-observer.svelte.js';
 
 	interface Props extends Omit<HTMLAttributes<HTMLLIElement>, 'class'> {
+		/** @default $props.id() */
+		id?: string;
 		value: string;
 		class?: ClassNameValue;
 		/** @default false */
@@ -16,6 +19,7 @@
 
 	let {
 		children,
+		id,
 		value,
 		class: className,
 		selected: _selected = false,
@@ -23,31 +27,46 @@
 		...restProps
 	}: Props = $props();
 
+	const uid = $props.id();
+
 	let parent = $state<HTMLUListElement>();
 	let ref = $state<HTMLLIElement>();
 
+	let focused = $state(false);
 	let selected = $derived(_selected);
-	const { container } = $derived(option({ selected, disabled }));
+	let _id = $derived(id ?? uid);
+
+	const { container } = $derived(option({ disabled, focused }));
+
+	useMutationObserver(
+		() => parent,
+		() => {
+			selected = value === parent!.getAttribute('data-selected-value');
+		},
+		{
+			attributes: true,
+			attributeFilter: ['data-selected-value']
+		}
+	);
+
+	useMutationObserver(
+		() => parent,
+		() => {
+			focused = _id === parent!.getAttribute('data-focused-id');
+		},
+		{
+			attributes: true,
+			attributeFilter: ['data-focused-id']
+		}
+	);
 
 	onMount(() => {
 		parent = ref?.parentElement as HTMLUListElement;
+		selected = value === parent.getAttribute('data-selected-value');
 	});
 
 	$effect(() => {
 		if (!parent) return;
-
-		const observer = new MutationObserver(() => {
-			selected = value === parent!.getAttribute('data-selected-value');
-		});
-
-		observer.observe(parent, {
-			attributes: true,
-			attributeFilter: ['data-selected-value']
-		});
-
-		selected = value === parent.getAttribute('data-selected-value');
-
-		return () => observer.disconnect();
 	});
 </script>
 
@@ -60,6 +79,7 @@ Designed to be a drop-in replacement for the native `option` element.
 
 <li
 	bind:this={ref}
+	id={_id}
 	role="option"
 	data-select-option
 	data-value={value}
