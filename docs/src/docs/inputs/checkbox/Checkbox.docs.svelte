fb50b058be49fa5ccf65144e995f5f23c00ed612
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-12 23:04:06 +0200

    wip

---
+++ docs/src/docs/inputs/checkbox/Checkbox.docs.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Playground from '$components/Playground.svelte';
+	import { script } from '$utils/playground';
+	import { Checkbox } from '@novaotp/components';
+
+	const code = {
+		short: `<Checkbox />
+<Checkbox checked />
+<Checkbox disabled />
+<Checkbox checked disabled />`,
+		expanded: `${script("import { Checkbox } from '@novaotp/components';")}
+
+<Checkbox />
+<Checkbox checked />
+<Checkbox disabled />
+<Checkbox checked disabled />`
+	};
+</script>
+
+<h1>Checkbox</h1>
+<p>Checkboxes allow the user to select one or more items from a set.</p>
+
+<h2>Basic Usage</h2>
+<Playground {code}>
+	<Checkbox />
+	<Checkbox checked />
+	<Checkbox disabled />
+	<Checkbox checked disabled />
+</Playground>
