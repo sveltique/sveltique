fdd9b8f708485deadfdd09d407cdaa3b7e8f91c5
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-15 23:30:52 +0200

    wip

---
+++ docs/src/docs/text-input/TextInput.docs.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Playground from '$components/Playground.svelte';
+	import { script } from '$utils/playground';
+	import { Badge, TextInput } from '@sveltique/components';
+</script>
+
+<h1>Text Input</h1>
+<p>Used for short, single-line text.</p>
+
+<h2>Basic Usage</h2>
+<p>Use it as a drop-in replacement for input <Badge>type="text"</Badge>.</p>
+<Playground
+	code={{
+		short: '<TextInput />',
+		expanded: `${script('import { TextInput } from "@sveltique/components";')}
+    
+<TextInput />`
+	}}
+>
+	<TextInput />
+</Playground>
