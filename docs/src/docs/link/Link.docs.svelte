e02f7c91fc351bfd7e8897b2562c93c6662bb96e
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-16 20:51:14 +0200

    wip

--- docs/src/docs/link/Link.docs.svelte
+++ docs/src/docs/link/Link.docs.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import Playground from '$components/Playground.svelte';
 	import { Badge, Link } from '@sveltique/components';
-	import { basicUsageCode } from './content.js';
+	import Playground from '$components/Playground.svelte';
+	import { basicUsageCode, externalCode, underlineCode } from './content.js';
 </script>
 
 <h1>Link</h1>
@@ -13,17 +13,31 @@
 	<Link href="#basic-usage">Go to the "Basic Usage" section</Link>
 </Playground>
 
+<h3>Underline</h3>
+<p>
+	By default, links are underlined to conform with <Link
+		href="https://webaim.org/blog/wcag-2-0-and-link-colors/"
+		external>accessbility guidelines</Link
+	>. However, you can change this behavior using the <Badge>underline</Badge> attribute.
+</p>
+<Playground code={underlineCode}>
+	<Link href="#">underline="always"</Link>
+	<Link href="#" underline="hover">underline="hover"</Link>
+	<Link href="#" underline="none">underline="none"</Link>
+</Playground>
+
 <h2>External links</h2>
 <p>
 	For external links, you can set the <Badge>external</Badge> attribute. It handles setting <Badge
 		>target="_blank"</Badge
-	> and <Badge>rel="noreferrer"</Badge>. You can override them individually if you need to.
+	>, <Badge>rel="noreferrer"</Badge>, and adds a small top right arrow icon (↗).
 </p>
-<Playground>
+<p>You can override them individually if you need to.</p>
+<Playground code={externalCode}>
 	<Link
 		href="https://developer.mozilla.org"
 		external
-		aria-label="To read more, see the MDN docs in a new tab."
+		title="To read more, see the MDN docs in a new tab."
 	>
 		See the MDN docs
 	</Link>
--- docs/src/docs/link/content.ts
+++ docs/src/docs/link/content.ts
@@ -10,3 +10,33 @@ export const basicUsageCode = {
     Go to the "Basic Usage" section
 </Link>`
 };
+
+export const underlineCode = {
+	short: `<Link href="#">underline="always"</Link>
+<Link href="#" underline="hover">underline="hover"</Link>
+<Link href="#" underline="none">underline="none"</Link>`,
+	expanded: `${script('import { Link } from "@sveltique/components";')}
+
+<Link href="#">underline="always"</Link>
+<Link href="#" underline="hover">underline="hover"</Link>
+<Link href="#" underline="none">underline="none"</Link>`
+};
+
+export const externalCode = {
+	short: `<Link
+    href="https://developer.mozilla.org"
+    external
+    title="To read more, see the MDN docs in a new tab."
+>
+    See the MDN docs
+</Link>`,
+	expanded: `${script('import { Link } from "@sveltique/components";')}
+
+<Link
+    href="https://developer.mozilla.org"
+    external
+    title="To read more, see the MDN docs in a new tab."
+>
+    See the MDN docs
+</Link>`
+};
