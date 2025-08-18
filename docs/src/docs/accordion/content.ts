42411533972d768c6f813d9f59900b8e71ed55c6
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-18 23:08:34 +0200

    wip

--- docs/src/docs/accordion/content.ts
+++ docs/src/docs/accordion/content.ts
@@ -1,8 +1,35 @@
 import { script } from '$utils/playground';
 
-export const defaultExpandCode = `${script('import { Accordion } from "@sveltique/components";')}
+export const basicCode = `${script('import { Accordion } from "@sveltique/components";')}
 
-<Accordion.Root defaultExpand>
+<Accordion.Root>
+    <Accordion.Item>
+        {#snippet header()}
+            Accordion 1
+        {/snippet}
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptate impedit deserunt amet officia qui tenetur
+            laboriosam consequatur incidunt, voluptatum atque.
+        </p>
+    </Accordion.Item>
+    <Accordion.Item>
+        {#snippet header()}
+            Accordion 2
+        {/snippet}
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eos sequi consectetur dolores libero tempora ducimus
+            perferendis iste qui voluptas. Cum!
+        </p>
+    </Accordion.Item>
+</Accordion.Root>`;
+
+export const expandIconCode = `${script('import { Accordion } from "@sveltique/components";')}
+
+<Accordion.Root>
     <Accordion.Item Icon={IconArrowDown}>
         {#snippet header()}
             Accordion 1
@@ -14,8 +41,34 @@ export const defaultExpandCode = `${script('import { Accordion } from "@sveltiqu
             laboriosam consequatur incidunt, voluptatum atque.
         </p>
     </Accordion.Item>
-
     <Accordion.Item Icon={IconCaretDownFilled}>
+        {#snippet header()}
+            Accordion 2
+        {/snippet}
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eos sequi consectetur dolores libero tempora ducimus
+            perferendis iste qui voluptas. Cum!
+        </p>
+    </Accordion.Item>
+</Accordion.Root>`;
+
+export const defaultExpandCode = `${script('import { Accordion } from "@sveltique/components";')}
+
+<Accordion.Root defaultExpand>
+    <Accordion.Item>
+        {#snippet header()}
+            Accordion 1
+        {/snippet}
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptate impedit deserunt amet officia qui tenetur
+            laboriosam consequatur incidunt, voluptatum atque.
+        </p>
+    </Accordion.Item>
+    <Accordion.Item>
         {#snippet header()}
             Accordion 2
         {/snippet}
