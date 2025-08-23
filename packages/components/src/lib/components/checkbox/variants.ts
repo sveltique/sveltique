d3cf0aa353f17a6929d9fffd8db40ab6faa20f76
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-24 01:03:20 +0200

    wip

--- packages/components/src/lib/components/checkbox/variants.ts
+++ packages/components/src/lib/components/checkbox/variants.ts
@@ -2,22 +2,22 @@ import { tv } from 'tailwind-variants';
 
 export const checkbox = tv({
 	base: [
-		'relative cursor-pointer rounded-sm border-2 bg-background border-muted-foreground text-primary',
-		'focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 dark:focus-visible:ring-offset-zinc-900 focus-visible:ring-blue-700 dark:focus-visible:ring-blue-300'
+		'relative cursor-pointer rounded-sm border-2 bg-background border-muted-foreground',
+		'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-background focus:ring-primary'
 	],
 	variants: {
 		checked: {
-			true: 'border-none'
+			true: 'border-none bg-primary'
 		},
 		disabled: {
-			true: 'border-zinc-300 dark:border-zinc-700 cursor-not-allowed'
+			true: 'border-muted cursor-not-allowed'
 		}
 	},
 	compoundVariants: [
 		{
 			checked: true,
 			disabled: true,
-			class: 'text-primary-disabled'
+			class: 'bg-primary-disabled'
 		}
 	]
 });
