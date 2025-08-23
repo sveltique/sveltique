73499e5bca2b470b215afa99530f5f99c562426a
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-23 20:40:27 +0200

    wip

--- packages/components/src/lib/components/separator/variants.ts
+++ packages/components/src/lib/components/separator/variants.ts
@@ -1,7 +1,7 @@
 import { tv, type VariantProps } from 'tailwind-variants';
 
 export const separator = tv({
-	base: 'relative bg-zinc-300 dark:bg-zinc-700',
+	base: 'relative bg-muted',
 	variants: {
 		orientation: {
 			horizontal: 'h-px w-full',
