876abe9d458fa9e4929167ea1a3ed1ddf96b6cf7
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-24 13:10:55 +0200

    improved badge

--- docs/content/components/browse/Badge.svelte
+++ docs/content/components/browse/Badge.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Badge } from '@sveltique/components';
+	import { badge, Badge } from '@sveltique/components';
 	import Playground from '$components/Playground.svelte';
 	import { script } from '$utils/playground';
 	import IconCircleDashedCheck from '@tabler/icons-svelte/icons/circle-dashed-check';
@@ -17,8 +17,26 @@
 	const numberCode = `${script('import { Badge } from "@sveltique/components";')}
     
 <Badge number>1</Badge>
-<Badge variant="outline" number>15+</Badge>
-<Badge variant="danger" number>99</Badge>`;
+<Badge variant="outline" number>15</Badge>
+<Badge variant="danger" number>99+</Badge>`;
+
+	const iconWithTextCode = `${script(`import { Badge } from '@sveltique/components';
+    import IconCircleDashedCheck from '@tabler/icons-svelte/icons/circle-dashed-check';
+    import IconTrash from '@tabler/icons-svelte/icons/trash';`)}
+    
+<Badge>
+    <IconCircleDashedCheck class="size-4" />
+    Verified
+</Badge>
+<Badge variant="danger">
+    <IconTrash class="size-4" />
+    Delete
+</Badge>`;
+
+	const interactiveBadgeCode = `${script("import { badge } from '@sveltique/components';")}
+
+<a href="/" class={badge()}>Link</a>
+<button class={badge({ variant: 'danger' })}>Button</button>`;
 </script>
 
 <h1>Badge</h1>
@@ -34,23 +52,31 @@
 <h3>Number</h3>
 <p>
 	If you want to use the badge with a number, you can set the <Badge>number</Badge> attribute to true.
+	It will fully round the badge.
 </p>
 <Playground code={numberCode}>
 	<Badge number>1</Badge>
-	<Badge variant="outline" number>15+</Badge>
-	<Badge variant="danger" number>99</Badge>
+	<Badge variant="outline" number>15</Badge>
+	<Badge variant="danger" number>99+</Badge>
 </Playground>
 
 <h2>Examples</h2>
 
 <h3>Icon with text</h3>
-<Playground>
+<Playground code={iconWithTextCode}>
 	<Badge>
-		<IconCircleDashedCheck class="size-4" />
+		<IconCircleDashedCheck aria-hidden="true" class="size-4" />
 		Verified
 	</Badge>
 	<Badge variant="danger">
-		<IconTrash class="size-4" />
+		<IconTrash aria-hidden="true" class="size-4" />
 		Delete
 	</Badge>
+</Playground>
+
+<h3>Interactive badge</h3>
+<p>You can make an interactive badge by using the <Badge>badge()</Badge> helper.</p>
+<Playground code={interactiveBadgeCode}>
+	<a href="/" class={badge()}>Link</a>
+	<button class={badge({ variant: 'danger' })}>Button</button>
 </Playground>
