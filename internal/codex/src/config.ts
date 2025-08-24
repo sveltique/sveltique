7833fa6516cce22dc9cd4c6d3ab407f12cdb64e2
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-24 18:14:22 +0200

    added ordering support in codex

--- internal/codex/src/config.ts
+++ internal/codex/src/config.ts
@@ -14,19 +14,49 @@ export function defineConfig(
  * @note It only supports top-level files.
  */
 export function fromFileSystem(dirPath: string, docsRoot: string = ''): SidebarItem[] {
-	const items: SidebarItem[] = [];
 	const entries = fs.readdirSync(path.join(docsRoot, dirPath), { withFileTypes: true });
+
+	let items: SidebarItem[] = [];
+	let nameOrders: { position: number; name: string; originalName: string }[] = [];
 
 	for (const entry of entries) {
 		// TODO : Support one day
 		if (entry.isDirectory()) continue;
 
+		const filename = entry.name.split('.').at(0)!;
+
+		// If true, uses the XX-NAME syntax
+		// Which means we have to follow the specified order
+		if (filename.includes('-')) {
+			const [position, name] = filename.split('-', 2);
+
+			nameOrders = [
+				...nameOrders,
+				{ position: Number(position), name, originalName: filename }
+			].sort((a, b) => a.position - b.position);
+		}
+
 		items.push({
-			name: entry.name.split('.').at(0)!,
+			name: filename,
 			path: path.join(entry.parentPath, `./${entry.name}`),
 			children: undefined
 		});
 	}
 
-	return items;
+	// Put ordered elements first, then sort alphabetically
+	const orderedItems: SidebarItem[] = [];
+
+	if (nameOrders.length > 0) {
+		// It is already sorted properly
+		for (const { originalName, name } of nameOrders) {
+			const sidebarItem = items.find((item) => item.name === originalName)!;
+
+			orderedItems.push({ ...sidebarItem, name });
+			items = items.filter((item) => item.name !== originalName);
+		}
+	}
+
+	orderedItems.push(...items);
+
+	return orderedItems;
 }
