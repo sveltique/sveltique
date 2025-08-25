d90edee006a0838fd868ec4e804924193899c150
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-26 00:05:19 +0200

    fix

--- docs/src/routes/docs/components/[...slugPath]/+page.ts
+++ docs/src/routes/docs/components/[...slugPath]/+page.ts
@@ -1,11 +1,24 @@
+import type { Component } from "svelte";
+
 export const ssr = false;
 
-export async function load({ data }) {
-	const { default: Component, metadata } = await import(/* @vite-ignore */ data.path);
+const modules = import.meta.glob<
+	boolean,
+	string,
+	{ default: Component; metadata?: Record<string, string> }
+>("../../../../../content/**/*.svelte");
 
+export async function load({ data }) {
 	return {
 		name: data.name,
-		metadata,
-		Component
+		...(await loadComponent(data.path))
 	};
+}
+
+async function loadComponent(path: string) {
+	const loader = modules[path];
+	if (!loader) throw new Error(`Module not found for ${path}`);
+
+	const { default: Component, metadata } = await loader();
+	return { Component, metadata };
 }
