d7f7dffd5aff7275c276c97abd18ff692dde64c6
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-26 22:32:35 +0200

    wip

---
+++ docs/content/components/getting-started/04-Ref.svelte
@@ -0,0 +1,19 @@
+<script>
+import { Badge } from "@sveltique/components";
+import CodeBlock from "$components/CodeBlock.svelte";
+import { script } from "$utils/playground";
+</script>
+
+<h1>Ref</h1>
+<p>Learn about the global ref attribute.</p>
+
+<h2>Basic Usage</h2>
+<p>Every Sveltique component exposes a <Badge>ref</Badge> attribute, returning a reference to the root element of a given component.</p>
+<p>By binding to it, you can make direct manipulations on the element.</p>
+<CodeBlock code={`${script(`import { Button } from '@sveltique/components';
+
+    let buttonRef = $state<HTMLButtonElement>();`)}
+    
+<Button bind:ref={buttonRef}>
+    I'm a button !
+</Button>`} />
