cae46fc0e81a0aa7da487b8cc6f7baa208e15851
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-26 22:21:28 +0200

    wip

--- packages/components/src/lib/components/text-input/TextInput.svelte
+++ packages/components/src/lib/components/text-input/TextInput.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 import type { HTMLInputAttributes } from "svelte/elements";
 import type { ClassNameValue } from "tailwind-merge";
+import type { WithRef } from "$lib/types.js";
 import { type TextInputVariants, textInput } from "./variants.js";
 
-interface Props extends Omit<HTMLInputAttributes, "class" | "value">, TextInputVariants {
+interface Props
+	extends Omit<HTMLInputAttributes, "class" | "value">,
+		TextInputVariants,
+		WithRef<HTMLElement | HTMLInputElement> {
 	class?: ClassNameValue;
 	value?: string;
 }
 
-let { class: className, value = $bindable(""), ...restProps }: Props = $props();
+let { class: className, ref = $bindable(), value = $bindable(""), ...restProps }: Props = $props();
 </script>
 
-<input bind:value type="text" class={textInput({ className })} {...restProps} />
+<input bind:this={ref} bind:value type="text" class={textInput({ className })} {...restProps} />
