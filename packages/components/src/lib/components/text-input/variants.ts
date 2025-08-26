b9be8441b02a269d7d61a4ee32c3937faac4b16c
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-26 22:01:16 +0200

    fix

--- packages/components/src/lib/components/text-input/variants.ts
+++ packages/components/src/lib/components/text-input/variants.ts
@@ -3,7 +3,7 @@ import { tv, type VariantProps } from "tailwind-variants";
 export const textInput = tv({
 	base: [
 		"relative h-11 w-full rounded-2xl border border-muted bg-transparent px-4 text-sm",
-		"focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary dark:focus:ring-offset-background"
+		"focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:ring-offset-background"
 	]
 });
 
