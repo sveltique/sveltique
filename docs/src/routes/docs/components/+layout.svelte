475b086db7107b48fccce5a5a32ed53881e42b1c
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-28 18:22:35 +0200

    wip

--- docs/src/routes/docs/components/+layout.svelte
+++ docs/src/routes/docs/components/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-import { Backdrop, button } from "@sveltique/components";
+import { Backdrop, button, Paper } from "@sveltique/components";
 import IconChevronRight from "@tabler/icons-svelte/icons/chevron-right";
 import IconMenuDeep from "@tabler/icons-svelte/icons/menu-deep";
 import { untrack } from "svelte";
+import { on } from "svelte/events";
 import { fly } from "svelte/transition";
 import { cnBase } from "tailwind-variants";
 import { page } from "$app/state";
@@ -15,6 +16,9 @@ const headings = setOnThisPageContext();
 let showMenu = $state(false);
 let showOnThisPage = $state(false);
 
+let showOnThisPageButtonRef = $state<HTMLButtonElement>();
+let showOnThisPageRef = $state<HTMLDivElement>();
+
 $effect(() => {
 	document.body.style.overflow = showMenu ? "hidden" : "auto";
 });
@@ -26,12 +30,35 @@ $effect(() => {
 		untrack(() => (showMenu = false));
 	}
 });
+
+$effect(() => {
+	if (!showOnThisPageButtonRef || !showOnThisPageRef) return;
+
+	return on(
+		window,
+		"click",
+		(event) => {
+			const target = event.target;
+
+			if (
+				!target ||
+				showOnThisPageButtonRef!.contains(target as Node) ||
+				showOnThisPageRef!.contains(target as Node)
+			) {
+				return;
+			}
+
+			showOnThisPage = false;
+		},
+		{ passive: true }
+	);
+});
 </script>
 
 <div class="relative flex min-h-[100vh] w-full flex-col lg:flex-row">
 	{@render componentsBar()}
-	<div class="md:px-18 relative w-full px-6 sm:px-12 lg:px-12 xl:px-32">
-		<div data-content class="lg:w-xl xl:w-2xl relative mx-auto pt-12">
+	<div class="relative w-full xl:flex-grow-[2] px-6 sm:px-12 xl:px-24 2xl:px-32">
+		<div data-content class="max-w-2xl xl:min-w-2xl relative mx-auto pt-12">
             {@render children()}
 		</div>
 	</div>
@@ -40,9 +67,9 @@ $effect(() => {
 
 {#snippet componentsBar()}
 	<aside
-		class="sticky left-0 top-16 z-50 hidden h-[calc(100vh-4rem)] items-start justify-end overflow-auto bg-zinc-50 p-6 lg:flex xl:w-full dark:bg-zinc-800"
+		class="sticky left-0 top-16 z-50 hidden h-[calc(100vh-4rem)] items-start justify-end overflow-auto p-6 lg:flex lg:pl-24 xl:pl-6 xl:w-full"
 	>
-		<nav class="relative flex min-w-[240px] flex-col gap-5 text-sm font-semibold">
+		<nav class="relative w-80 flex flex-col gap-5 text-sm font-semibold">
 			{#each data.sidebar as rootItem, index (index)}
 				<div class="relative flex w-full flex-col gap-2.5">
 					<span>{rootItem.name}</span>
@@ -80,6 +107,7 @@ $effect(() => {
 				Menu
 			</button>
 			<button
+                bind:this={showOnThisPageButtonRef}
 				onclick={() => (showOnThisPage = true)}
 				class="relative flex h-full cursor-pointer items-center gap-2.5"
 			>
@@ -87,6 +115,52 @@ $effect(() => {
 				<IconChevronRight class="h-4 w-4" />
 			</button>
 		</nav>
+
+        {#if showOnThisPage}
+            <div transition:fly={{ y: -10, duration: 150 }}>
+                <Paper
+                    bind:ref={showOnThisPageRef}
+                    variant="outline"
+                    class="absolute top-[calc(100%-8px)] left-4 right-4 flex w-[calc(100%-2rem)] p-6 shadow-md"
+                >
+                    <nav
+                        class="relative w-full flex-col items-start justify-between"
+                    >
+                        <div
+                            class="relative flex h-full w-full flex-col items-start justify-start gap-2.5 text-sm font-medium"
+                        >
+                            <div role="heading" aria-level="2" class="font-bold">On This Page</div>
+                            {#each headings.current as { id, label, items } (id)}
+                                <a href="#{id}" onclick={() => (showOnThisPage = false)} class="text-muted-foreground">
+                                    {label}
+                                </a>
+                                {#if items.length > 0}
+                                    <div class="relative flex w-full flex-col gap-2.5 pl-5">
+                                        {#each items as item (item.id)}
+                                            <a
+                                                href="#{item.id}"
+                                                onclick={() => (showOnThisPage = false)}
+                                                class="text-muted-foreground"
+                                            >
+                                                {item.label}
+                                            </a>
+                                        {/each}
+                                    </div>
+                                {/if}
+                            {/each}
+                        </div>
+                        <!-- svelte-ignore a11y_invalid_attribute -->
+                        <a
+                            href="#"
+                            onclick={() => (showOnThisPage = false)}
+                            class={button({ variant: 'outline', class: 'w-full text-sm' })}
+                        >
+                            Return to top
+                        </a>
+                    </nav>
+                </Paper>
+            </div>
+        {/if}
 	</header>
 
 	{#if showMenu}
@@ -98,7 +172,7 @@ $effect(() => {
 			<nav
 				transition:fly={{ x: -100, duration: 150 }}
 				class={cnBase([
-                    "relative flex h-[100vh] w-4/5 flex-col items-start justify-start gap-5 overflow-auto bg-background",
+                    "relative flex h-[100vh] w-4/5 max-w-[320px] flex-col items-start justify-start gap-5 overflow-auto bg-background",
                     "p-6 shadow-[0_0_12px_4px_rgba(0,0,0,0.15)] text-sm"
                 ])}
 			>
@@ -123,52 +197,6 @@ $effect(() => {
                         </div>
                     </div>
                 {/each}
-			</nav>
-		</Backdrop>
-	{/if}
-
-	{#if showOnThisPage}
-		<Backdrop
-			onClick={() => (showOnThisPage = false)}
-			z={100}
-			class="flex items-start justify-end lg:hidden"
-		>
-			<nav
-				transition:fly={{ x: 100, duration: 150 }}
-				class="relative flex h-[100vh] w-4/5 flex-col items-start justify-between bg-white p-6 shadow-[0_0_12px_4px_rgba(0,0,0,0.15)] dark:bg-zinc-800"
-			>
-				<div
-					class="relative flex h-full w-full flex-col items-start justify-start gap-2.5 text-sm font-medium"
-				>
-					<div role="heading" aria-level="2" class="font-bold">On This Page</div>
-					{#each headings.current as { id, label, items } (id)}
-						<a href="#{id}" onclick={() => (showOnThisPage = false)} class="text-muted-foreground">
-                            {label}
-                        </a>
-
-						{#if items.length > 0}
-							<div class="relative flex w-full flex-col gap-2.5 pl-5">
-								{#each items as item (item.id)}
-									<a
-                                        href="#{item.id}"
-                                        onclick={() => (showOnThisPage = false)}
-                                        class="text-muted-foreground"
-                                    >
-                                        {item.label}
-                                    </a>
-								{/each}
-							</div>
-						{/if}
-					{/each}
-				</div>
-				<!-- svelte-ignore a11y_invalid_attribute -->
-				<a
-                    href="#"
-                    onclick={() => (showOnThisPage = false)}
-                    class={button({ variant: 'outline', class: 'w-full self-end text-sm' })}
-                >
-					Return to top
-				</a>
 			</nav>
 		</Backdrop>
 	{/if}
