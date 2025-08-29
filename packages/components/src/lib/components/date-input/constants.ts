282e4d3bad0871d6f2c2d1adefdc003a9d3a7856
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-29 19:54:44 +0200

    removed date input for now

--- packages/components/src/lib/components/date-input/constants.ts
+++
@@ -1,9 +0,0 @@
-export const WEEK_DAYS = [
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
-	"Sunday"
-];
--- packages/components/src/lib/components/date-input/variants.ts
+++
@@ -1,8 +0,0 @@
-import { tv } from "tailwind-variants";
-
-export const dateInput = tv({
-	slots: {
-		input: "relative flex items-center gap-2.5 h-11 rounded-large border border-zinc-300 px-4 text-sm dark:border-zinc-300-dark cursor-pointer",
-		grid: "grid grid-cols-7 gap-1 mt-2"
-	}
-});
