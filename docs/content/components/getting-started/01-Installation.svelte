72f09d0500008d7fd000b3057d4366418d93cff2
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-08-31 19:48:24 +0200

    added warning about not using in production

--- docs/content/components/getting-started/01-Installation.svelte
+++ docs/content/components/getting-started/01-Installation.svelte
@@ -10,10 +10,12 @@ import CodeBlock from "$components/CodeBlock.svelte";
 
 <h1 id="installation" class="text-3xl font-bold">Installation</h1>
 <p>Install Sveltique components library.</p>
-<Alert type="warning">
-    Until Sveltique Components reaches 1.0, there may be breaking changes between minor versions.
-    It is advised to pin your current version and manually ensure that your application is still
-    working after updating.
+<Alert type="warning" class="mb-4">
+    Until 1.0, there may be breaking changes between minor versions. It is advised to pin your current
+    version and manually ensure that your application is still working after updating.
+</Alert>
+<Alert type="danger">
+    This library is not suited for production-level code.
 </Alert>
 
 <h2 id="default-installation">Default installation</h2>
