ecbc66b8498582764e9e2e2211e4646f04b57161
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-09-06 18:17:24 +0200

    wip

--- packages/components/src/lib/components/otp/index.ts
+++ packages/components/src/lib/components/otp/index.ts
@@ -1,1 +1,6 @@
-export { default as OTP } from "./OTP.svelte";
+import Root from "./OTP.svelte";
+
+// @ts-ignore
+export const OTP: typeof Root & { Root: typeof Root } = Root;
+// @ts-ignore
+OTP.Root = Root;
