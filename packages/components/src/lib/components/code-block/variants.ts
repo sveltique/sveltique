817d3f769980a5e331dcff5ca957abae2759fe70
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-09-13 20:33:01 +0200

    fix: `Codeblock`'s pre and code take the full height of the container (#29)

--- packages/components/src/lib/components/code-block/variants.ts
+++ packages/components/src/lib/components/code-block/variants.ts
@@ -2,11 +2,11 @@ import { tv, type VariantProps } from "tailwind-variants";
 
 export const codeBlock = tv({
 	slots: {
-		container: "relative w-full text-sm rounded-large",
+		container: "relative w-full text-sm rounded-large flex flex-col overflow-hidden",
 		header: "relative w-full flex items-center justify-between px-5 py-2.5",
 		filename: "font-codeblock",
-		pre: "code-block relative w-full flex flex-col overflow-x-auto",
-		code: "contents flex flex-col flex-nowrap py-5 font-codeblock"
+		pre: "code-block relative w-full grow flex flex-col overflow-auto",
+		code: "contents flex flex-col flex-nowrap py-5 grow font-codeblock"
 	}
 });
 
