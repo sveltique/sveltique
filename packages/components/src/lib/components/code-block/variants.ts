46e4137c6d98e66801e32a291ea4b443acb2f853
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-09-13 20:43:38 +0200

    fix: removed Codeblock container overflow-hidden

--- packages/components/src/lib/components/code-block/variants.ts
+++ packages/components/src/lib/components/code-block/variants.ts
@@ -2,7 +2,7 @@ import { tv, type VariantProps } from "tailwind-variants";
 
 export const codeBlock = tv({
 	slots: {
-		container: "relative w-full text-sm rounded-large flex flex-col overflow-hidden",
+		container: "relative w-full text-sm rounded-large flex flex-col",
 		header: "relative w-full flex items-center justify-between px-5 py-2.5",
 		filename: "font-codeblock",
 		pre: "code-block relative w-full grow flex flex-col overflow-auto",
