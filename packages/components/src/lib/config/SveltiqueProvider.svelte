f9c56b4fbac2c9d67594b4db9fe8877cf1e32043
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-10-04 10:18:07 +0200

    feat: added a global provider

---
+++ packages/components/src/lib/config/SveltiqueProvider.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+import type { Snippet } from "svelte";
+import { type ProviderContextOptions, setProviderContext } from "./context.svelte.js";
+
+interface Props extends ProviderContextOptions {
+	/**
+	 * The children content to render.
+	 * @default —
+	 */
+	children?: Snippet;
+}
+
+let { animation = "system", children }: Props = $props();
+
+setProviderContext({ animation });
+</script>
+
+<!--
+@component
+A provider to handle global configs.
+@see https://sveltique.dev/docs/components/configuration/sveltique-provider
+-->
+
+{@render children?.()}
---
+++ packages/components/src/lib/config/index.ts
@@ -0,0 +1,8 @@
+export {
+	type Animation,
+	getProviderContext,
+	type OverrideProviderContextOptions,
+	type ProviderContextOptions
+} from "./context.svelte.js";
+export { default as SveltiqueProvider } from "./SveltiqueProvider.svelte";
+export { resolveAnimation } from "./utils.js";
