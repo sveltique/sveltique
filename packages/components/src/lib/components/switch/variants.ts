b3f046566bdb77a59d83c7cd16e05fdcd14a2c2b
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-10-22 18:23:19 +0200

    fix(switch): component is now motion-safe

--- packages/components/src/lib/components/switch/variants.ts
+++ packages/components/src/lib/components/switch/variants.ts
@@ -1,37 +1,37 @@
 import { tv, type VariantProps } from "tailwind-variants";
 
 export const switchTv = tv({
-	slots: {
-		container: "relative h-6 w-12 cursor-pointer rounded-full bg-muted",
-		thumb: "absolute top-0.5 left-0.5 aspect-square h-[calc(100%-4px)] rounded-full bg-background pointer-events-none duration-150 ease-in-out"
-	},
-	variants: {
-		checked: {
-			true: {
-				container: "bg-primary",
-				thumb: "translate-x-[calc(100%+4px)]"
-			}
-		},
-		disabled: {
-			true: {
-				container: "bg-muted/50 cursor-not-allowed"
-			},
-			false: {
-				container:
-					"focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:ring-offset-background"
-			}
-		}
-	},
-	compoundVariants: [
-		{
-			checked: true,
-			disabled: true,
-			class: {
-				container: "bg-primary-disabled",
-				thumb: "bg-muted"
-			}
-		}
-	]
+    slots: {
+        container: "relative h-6 w-12 cursor-pointer rounded-full bg-muted",
+        thumb: "absolute top-0.5 left-0.5 aspect-square h-[calc(100%-4px)] rounded-full bg-background pointer-events-none motion-safe:duration-150 ease-in-out"
+    },
+    variants: {
+        checked: {
+            true: {
+                container: "bg-primary",
+                thumb: "translate-x-[calc(100%+4px)]"
+            }
+        },
+        disabled: {
+            true: {
+                container: "bg-muted/50 cursor-not-allowed"
+            },
+            false: {
+                container:
+                    "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:ring-offset-background"
+            }
+        }
+    },
+    compoundVariants: [
+        {
+            checked: true,
+            disabled: true,
+            class: {
+                container: "bg-primary-disabled",
+                thumb: "bg-muted"
+            }
+        }
+    ]
 });
 
 export type SwitchVariants = VariantProps<typeof switchTv>;
