324fd79d1edf637c02f599297106f49d1850b9f8
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-10-22 18:21:56 +0200

    fix(skeleton): component is now motion-safe

--- packages/components/src/lib/components/skeleton/variants.ts
+++ packages/components/src/lib/components/skeleton/variants.ts
@@ -1,16 +1,16 @@
 import { tv, type VariantProps } from "tailwind-variants";
 
 export const skeleton = tv({
-  base: "rounded-2xl bg-muted",
-  variants: {
-    animation: {
-      pulse: "animate-pulse",
-      none: "",
+    base: "rounded-2xl bg-muted",
+    variants: {
+        animation: {
+            pulse: "motion-safe:animate-pulse",
+            none: "",
+        },
     },
-  },
-  defaultVariants: {
-    animation: "pulse",
-  },
+    defaultVariants: {
+        animation: "pulse",
+    },
 });
 
 export type SkeletonVariants = VariantProps<typeof skeleton>;
