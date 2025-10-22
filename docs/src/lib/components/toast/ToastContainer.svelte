a017dd8b701082bc42b78893d02af0450796fae5
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-10-22 18:45:43 +0200

    docs(pattern): toast pattern is now motion-safe

--- docs/src/lib/components/toast/ToastContainer.svelte
+++ docs/src/lib/components/toast/ToastContainer.svelte
@@ -2,6 +2,7 @@
 
 <script lang="ts">
 import { Toast } from "@sveltique/components";
+import { motionSafe } from "@sveltique/components/utils";
 import { flip } from "svelte/animate";
 import { fly } from "svelte/transition";
 import { toasts } from "$lib/contexts/toast.svelte";
@@ -10,10 +11,13 @@ import { toasts } from "$lib/contexts/toast.svelte";
 <div
     role="status"
 	aria-live="polite"
-	class="fixed right-6 bottom-6 z-[1000] flex w-[calc(100%-3rem)] sm:max-w-xs flex-col justify-center gap-2"
+	class="right-6 bottom-6 z-[1000] fixed flex flex-col justify-center gap-2 w-[calc(100%-3rem)] sm:max-w-xs"
 >
 	{#each toasts.current as { id, type, content } (id)}
-		<div animate:flip={{ duration: 150 }} transition:fly={{ x: 50, duration: 200 }}>
+		<div
+            animate:flip={{ duration: motionSafe(0, 150) }}
+            transition:fly={{ x: 50, duration: motionSafe(0, 200) }}
+        >
             <Toast {type}>
                 {content}
             </Toast>
