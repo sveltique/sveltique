201714574a7c36f7f32727fb10668beede51846c
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-10-29 22:16:08 +0100

    docs(backdrop): removed drawer example (#73)

--- docs/content/components/browse/Backdrop.svelte
+++ docs/content/components/browse/Backdrop.svelte
@@ -6,17 +6,11 @@ export const metadata = {
 </script>
 
 <script>
-import { Backdrop, Button, Field, TextArea, TextInput } from "@sveltique/components";
-import { slide } from "svelte/transition";
+import { Backdrop, Button } from "@sveltique/components";
 import Playground from "$components/Playground.svelte";
 import { script } from "$utils/playground";
 
 let showBackdrop = $state(false);
-let showDrawer = $state(false);
-
-function onsubmit() {
-    showDrawer = false
-}
 
 const code = `${script(`import { Backdrop, Button } from '@sveltique/components';
 
@@ -32,68 +26,6 @@ const code = `${script(`import { Backdrop, Button } from '@sveltique/components'
         z={1000}
     />
 {/if}`;
-
-const exampleDrawerCode = `${script(`import {
-        Backdrop,
-        Button,
-        Field,
-        TextArea,
-        TextInput
-    } from '@sveltique/components';
-    import { slide } from "svelte/transition";
-
-    let showDrawer = $state(false);`)}
-
-<Button onclick={() => (showDrawer = true)}>
-    Open Drawer
-</Button>
-
-{#if showDrawer}
-    <Backdrop
-        onclick={() => (showDrawer = false)}
-        z={1000}
-    >
-        <div
-            transition:slide={{ axis: "y", duration: 150 }}
-            class={[
-                "fixed bottom-0 left-0 w-full shadow-[0_0_4px_8px_rgba(0,0,0,0.2)] p-6 rounded-t-2xl",
-                "dark:bg-[color-mix(in_oklch,var(--background),var(--foreground)_5%)]"
-            ]}
-        >
-            <form
-                {onsubmit}
-                class="relative w-full max-w-lg mx-auto flex flex-col gap-6 sm:py-6"
-            >
-                <h2 class="font-bold text-2xl">
-                    Bug Report
-                </h2>
-                <p class="text-muted-foreground">
-                    Please provide a brief title and a description of the issue you encountered.
-                </p>
-                <Field label="Title">
-                    {#snippet input({ props })}
-                        <TextInput
-                            placeholder="Backdrop component does not work..."
-                            {...props}
-                        />
-                    {/snippet}
-                </Field>
-                <Field label="Description">
-                    {#snippet input({ props })}
-                        <TextArea
-                            placeholder="I thought it would close when I pressed Escape..."
-                            rows={4}
-                            {...props}
-                        />
-                    {/snippet}
-                </Field>
-                <Button type="submit">
-                    Submit
-                </Button>
-            </form>
-        </div>
-    </Backdrop>
-{/if}`
 </script>
 
 <h1 id="backdrop">Backdrop</h1>
@@ -109,48 +41,4 @@ const exampleDrawerCode = `${script(`import {
 	{#if showBackdrop}
 		<Backdrop onclick={() => (showBackdrop = false)} z={1000} />
 	{/if}
-</Playground>
-
-<h2 id="example">Examples</h2>
-
-<h3 id="drawer">Drawer</h3>
-<p>An example of a sliding drawer layered above a backdrop, suitable for forms or panels.</p>
-<Playground code={exampleDrawerCode}>
-    <Button onclick={() => (showDrawer = true)}>Open Drawer</Button>
-
-    {#if showDrawer}
-        <Backdrop onclick={() => (showDrawer = false)} z={1000}>
-            <div
-                transition:slide={{ axis: "y", duration: 150 }}
-                class={[
-                    "fixed bottom-0 left-0 w-full shadow-[0_0_4px_8px_rgba(0,0,0,0.2)] p-6 rounded-t-2xl",
-                    "dark:bg-[color-mix(in_oklch,var(--background),var(--foreground)_5%)]"
-                ]}
-            >
-                <form {onsubmit} class="relative w-full max-w-lg mx-auto flex flex-col gap-6 sm:py-6">
-                    <h2 class="font-bold text-2xl">Bug Report</h2>
-                    <p class="text-muted-foreground">
-                        Please provide a brief title and a description of the issue you encountered.
-                    </p>
-                    <Field label="Title">
-                        {#snippet input({ props })}
-                            <TextInput placeholder="Backdrop component does not work..." {...props} />
-                        {/snippet}
-                    </Field>
-                    <Field label="Description">
-                        {#snippet input({ props })}
-                            <TextArea
-                                placeholder="I thought it would close when I pressed Escape..."
-                                rows={4}
-                                {...props}
-                            />
-                        {/snippet}
-                    </Field>
-                    <Button type="submit">
-                        Submit
-                    </Button>
-                </form>
-            </div>
-        </Backdrop>
-    {/if}
 </Playground>
