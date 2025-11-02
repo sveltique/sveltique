caee8f281abd54d7bc7ba29a308b0796ea0f51f2
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-11-02 12:37:25 +0100

    feat(paper): added elevated variant (#76)
    * feat(paper): added elevated variant
    
    * docs: updated theme
    
    * chore: added changeset

--- packages/components/src/lib/components/paper/Paper.svelte
+++ packages/components/src/lib/components/paper/Paper.svelte
@@ -6,22 +6,44 @@ import { type PaperVariants, paper } from "./variants.js";
 export interface PaperProps
 	extends ReplaceWithTWMergeClass<HTMLAttributes<HTMLDivElement>>,
 		PaperVariants,
-		WithRef<HTMLDivElement> {}
+		WithRef<HTMLDivElement> {
+	/**
+	 * Establish a hierarchy between the different elevated surfaces.
+	 *
+	 * Has no effect if `variant` is not set to `"elevated"`.
+	 * @default 1
+	 */
+	elevation?: PaperVariants["elevation"];
+	/**
+	 * The surface style of the paper.
+	 * @default "outline"
+	 */
+	variant?: PaperVariants["variant"];
+}
 
 let {
 	children,
 	class: className = undefined,
+	elevation = 1,
 	ref = $bindable(),
+	variant = "outline",
 	...restProps
 }: PaperProps = $props();
 </script>
 
 <!--
 @component
-A surface to organize related content and, when needed, display actions in a consistent way.
+A surface to organize related content together.
 @see https://sveltique.dev/docs/components/browse/paper
 -->
 
-<div bind:this={ref} data-paper class={paper({ className })} {...restProps}>
-	{@render children?.()}
+<div
+    bind:this={ref}
+    data-paper
+    data-variant={variant}
+    data-elevation={elevation}
+    class={paper({ variant, elevation, className })}
+    {...restProps}
+>
+    {@render children?.()}
 </div>
