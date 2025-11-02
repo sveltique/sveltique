2cefd25d83e86128f4d1f6e5297d9507a1d79555
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-11-02 10:53:21 +0100

    feat(paper): added elevated variant

--- docs/content/components/browse/Paper.svelte
+++ docs/content/components/browse/Paper.svelte
@@ -1,18 +1,40 @@
 <script module>
 export const metadata = {
-	description:
-		"A surface to organize related content and, when needed, display actions in a consistent way."
+	description: "A surface to organize related content together."
 };
 </script>
 
 <script lang="ts">
-import { AspectRatio, Link, Paper } from "@sveltique/components";
+import { Alert, AspectRatio, Badge, Link, Paper, type PaperProps } from "@sveltique/components";
 import Playground from "$components/Playground.svelte";
 import { script } from "$utils/playground";
 
-const basicCode = `${script('import { AspectRatio, Paper } from "@sveltique/components";')}
+const basicUsageCode = `${script('import { Paper } from "@sveltique/components";')}
 
-<Paper class="w-full flex flex-col max-w-xs overflow-hidden">
+<Paper class="px-6 py-4 text-sm text-center">
+    Outline
+</Paper>
+<Paper variant="elevated" class="px-6 py-4 text-sm text-center">
+    Elevated
+</Paper>`;
+
+const elevationCode = `${script('import { Paper } from "@sveltique/components";')}
+
+<div class="gap-4 grid grid-cols-[repeat(1,minmax(0,200px))] sm:grid-cols-[repeat(2,minmax(0,200px))]">
+    {#each { length: 4 } as _, elevation (elevation)}
+        <Paper
+            variant="elevated"
+            {elevation}
+            class="p-6 text-sm text-center"
+        >
+            elevation = {elevation}
+        </Paper>
+    {/each}
+</div>`;
+
+const exampleCardCode = `${script('import { AspectRatio, Paper } from "@sveltique/components";')}
+
+<Paper class="flex flex-col w-full max-w-xs overflow-hidden">
     <AspectRatio ratio={16 / 9}>
         <img
             src="/images/cat.webp"
@@ -20,7 +42,7 @@ const basicCode = `${script('import { AspectRatio, Paper } from "@sveltique/comp
             class="w-full h-full object-cover"
         />
     </AspectRatio>
-    <div class="relative w-full flex flex-col items-start gap-3 p-4">
+    <div class="relative flex flex-col items-start gap-3 p-4 w-full">
         <h3 class="font-bold text-foreground">
             Cats are the cutest!
         </h3>
@@ -32,11 +54,50 @@ const basicCode = `${script('import { AspectRatio, Paper } from "@sveltique/comp
 </script>
 
 <h1 id="paper">Paper</h1>
-<p>A surface to organize related content and, when needed, display actions in a consistent way.</p>
+<p>A surface to organize related content together.</p>
 
 <h2 id="basic-usage">Basic Usage</h2>
-<Playground code={basicCode} class="flex-col">
-	<Paper class="w-full flex flex-col max-w-xs overflow-hidden">
+<p>A paper has two variants : outline (default), and elevated.</p>
+<Playground code={basicUsageCode}>
+    <Paper class="px-6 py-4 text-sm text-center">
+        Outline
+    </Paper>
+    <Paper variant="elevated" class="px-6 py-4 text-sm text-center">
+        Elevated
+    </Paper>
+</Playground>
+
+<h2 id="customization">Customization</h2>
+
+<h3 id="elevation">Elevation</h3>
+<p>
+    Use the <Badge variant="secondary">elevation</Badge> property to control the distance of the paper
+    from the background.
+</p>
+<Alert class="mb-4">
+    This property has no effect if the <Badge variant="secondary">
+        variant
+    </Badge> is not set to <Badge variant="secondary">elevated</Badge>.
+</Alert>
+<Playground code={elevationCode}>
+    <div class="gap-4 grid grid-cols-[repeat(1,minmax(0,200px))] sm:grid-cols-[repeat(2,minmax(0,200px))]">
+        {#each { length: 4 } as _, elevation (elevation)}
+            <Paper
+                variant="elevated"
+                elevation={elevation as PaperProps["elevation"]}
+                class="p-6 text-sm text-center"
+            >
+                elevation = {elevation}
+            </Paper>
+        {/each}
+    </div>
+</Playground>
+
+<h2 id="examples">Examples</h2>
+
+<h3 id="card">Card</h3>
+<Playground code={exampleCardCode} class="flex-col">
+    <Paper class="flex flex-col w-full max-w-xs overflow-hidden">
         <AspectRatio ratio={16 / 9}>
             <img
                 src="/images/cat.webp"
@@ -44,13 +105,9 @@ const basicCode = `${script('import { AspectRatio, Paper } from "@sveltique/comp
                 class="w-full h-full object-cover"
             />
         </AspectRatio>
-        <div class="relative w-full flex flex-col items-start gap-3 p-4">
-            <h3 class="font-bold text-foreground">
-                Cats are the cutest!
-            </h3>
-            <p class="text-sm">
-                And yes, I prefer cats to dogs.
-            </p>
+        <div class="relative flex flex-col items-start gap-3 p-4 w-full">
+            <h3 class="font-bold text-foreground">Cats are the cutest!</h3>
+            <p class="text-sm">And yes, I prefer cats to dogs.</p>
         </div>
     </Paper>
     <p class="text-sm italic">
@@ -58,6 +115,8 @@ const basicCode = `${script('import { AspectRatio, Paper } from "@sveltique/comp
             href="https://unsplash.com/photos/three-assorted-color-kittens-rplhB9mYF48"
             aria-label="See the original image of the cats from Unsplash in another tab."
             external
-        >Unsplash</Link>
+        >
+            Unsplash
+        </Link>
     </p>
 </Playground>
--- packages/components/src/lib/components/paper/variants.ts
+++ packages/components/src/lib/components/paper/variants.ts
@@ -1,7 +1,50 @@
 import { tv, type VariantProps } from "tailwind-variants";
 
 export const paper = tv({
-  base: "relative rounded-2xl bg-background border border-muted",
+    base: "relative rounded-2xl bg-background",
+    variants: {
+        variant: {
+            outline: "border border-muted",
+            elevated: "",
+        },
+        elevation: {
+            0: "",
+            1: "",
+            2: "",
+            3: "",
+        },
+    },
+    compoundVariants: [
+        {
+            elevation: [1, 2, 3],
+            variant: "elevated",
+            class: "not-dark:border not-dark:border-muted/50",
+        },
+        {
+            elevation: 0,
+            variant: "elevated",
+            class: "shadow-none",
+        },
+        {
+            elevation: 1,
+            variant: "elevated",
+            class: "shadow-sm dark:bg-background-soft",
+        },
+        {
+            elevation: 2,
+            variant: "elevated",
+            class: "shadow-md dark:bg-background-softer",
+        },
+        {
+            elevation: 3,
+            variant: "elevated",
+            class: "shadow-lg dark:bg-background-softest",
+        },
+    ],
+    defaultVariants: {
+        variant: "outline",
+        elevation: 1,
+    },
 });
 
 export type PaperVariants = VariantProps<typeof paper>;
